e21f968750ee6237d7f6beaecab61a24a38093dc
Mika Reich <mikareich0@gmail.com>
2022-07-07 16:27:03 +0200

    init game screen

---
+++ src/ts/constants.ts
@@ -0,0 +1,3 @@
+export const COLS = 100
+export const ROWS = 75
+export const GAME_SCREEN_RATIO = ROWS / COLS
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,10 @@
 import '../scss/index.scss'
+import { GAME_SCREEN_RATIO } from './constants'
 
-const test: number = 1
+const gameScreenCANVAS = <HTMLCanvasElement>document.getElementById('gameScreen')!
 
-console.log(test)
-
-export {}
+// set width and height of canvas to fill the screen
+const width = window.innerWidth
+const height = width * GAME_SCREEN_RATIO
+gameScreenCANVAS.width = window.innerWidth
+gameScreenCANVAS.height = height
